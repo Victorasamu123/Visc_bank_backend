4fc4c14adf26def0368fc610ff190bca8202c6ed
Asamu victor <asamuvictor03@gmail.com>
2022-12-15 21:33:56 +0100

    updated app

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+require("dotenv").config();
 app.use(cors())
 app.use(express.urlencoded({extended:true}));
 app.use(express.json())
-require("dotenv").config();
 const URI =process.env.URI
 mongoose.connect(URI,(err)=>{
     if (err) {
